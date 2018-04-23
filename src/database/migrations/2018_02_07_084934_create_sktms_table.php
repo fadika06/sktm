3f9f78f98f0314e17b3bd6afd84b30e0cddf99b4
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-04-23 15:58:19 +0700

    Update migration

--- src/database/migrations/2018_02_07_084934_create_sktms_table.php
+++ src/database/migrations/2018_02_07_084934_create_sktms_table.php
@@ -14,11 +14,11 @@ class CreateSktmsTable extends Migration
 	{
 		Schema::create('sktms', function(Blueprint $table) {
 			$table->increments('id');
-			$table->integer('nomor_un');
-			$table->integer('user_id');
+			$table->string('nomor_un');
 			$table->integer('master_sktm_id');
 			$table->string('no_sktm');
 			$table->integer('nilai');
+			$table->integer('user_id');
 			$table->timestamps();
 			$table->softDeletes();
 		});
