9e623b897178cd85e82a234b6ca6fd28e043bf76
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-04-23 15:54:54 +0700

    Fix add publish all comman and reorganize file

---
+++ src/config/master-sktm.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Master SKTM Config
+    |--------------------------------------------------------------------------
+    |
+    | The following config lines are used for development of package
+    | Bantenprov/Sktm
+    |
+    */
+
+    'key' => 'value'
+
+];
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,45 +1,49 @@
 <?php
 
-Route::group(['prefix' => 'api/sktm', 'middleware' => ['web']], function() {
-    $controllers = (object) [
-        'index'     => 'Bantenprov\Sktm\Http\Controllers\SktmController@index',
-        'create'    => 'Bantenprov\Sktm\Http\Controllers\SktmController@create',
-        'show'      => 'Bantenprov\Sktm\Http\Controllers\SktmController@show',
-        'store'     => 'Bantenprov\Sktm\Http\Controllers\SktmController@store',
-        'edit'      => 'Bantenprov\Sktm\Http\Controllers\SktmController@edit',
-        'update'    => 'Bantenprov\Sktm\Http\Controllers\SktmController@update',
-        'destroy'   => 'Bantenprov\Sktm\Http\Controllers\SktmController@destroy',
-
+Route::group(['prefix' => 'api/master-sktm', 'middleware' => ['web']], function() {
+    $class          = 'Bantenprov\Sktm\Http\Controllers\MasterSktmController';
+    $name           = 'master-sktm';
+    $controllers    = (object) [
+        'index'     => $class.'@index',
+        'get'       => $class.'@get',
+        'create'    => $class.'@create',
+        'show'      => $class.'@show',
+        'store'     => $class.'@store',
+        'edit'      => $class.'@edit',
+        'update'    => $class.'@update',
+        'destroy'   => $class.'@destroy',
     ];
 
-    Route::get('/',             $controllers->index)->name('sktm.index');
-    Route::get('/create',       $controllers->create)->name('sktm.create');
-    Route::get('/{id}',         $controllers->show)->name('sktm.show');
-    Route::post('/',            $controllers->store)->name('sktm.store');
-    Route::get('/{id}/edit',    $controllers->edit)->name('sktm.edit');
-    Route::put('/{id}',         $controllers->update)->name('sktm.update');
-    Route::delete('/{id}',      $controllers->destroy)->name('sktm.destroy');
-
+    Route::get('/',             $controllers->index)->name($name.'.index');
+    Route::get('/get',          $controllers->get)->name($name.'.get');
+    Route::get('/create',       $controllers->create)->name($name.'.create');
+    Route::get('/{id}',         $controllers->show)->name($name.'.show');
+    Route::post('/',            $controllers->store)->name($name.'.store');
+    Route::get('/{id}/edit',    $controllers->edit)->name($name.'.edit');
+    Route::put('/{id}',         $controllers->update)->name($name.'.update');
+    Route::delete('/{id}',      $controllers->destroy)->name($name.'.destroy');
 });
 
-Route::group(['prefix' => 'api/master-sktm', 'middleware' => ['web']], function() {
-    $controllers = (object) [
-        'index'     => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@index',
-        'create'    => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@create',
-        'show'      => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@show',
-        'store'     => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@store',
-        'edit'      => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@edit',
-        'update'    => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@update',
-        'destroy'   => 'Bantenprov\Sktm\Http\Controllers\MasterSktmController@destroy',
-
+Route::group(['prefix' => 'api/sktm', 'middleware' => ['web']], function() {
+    $class          = 'Bantenprov\Sktm\Http\Controllers\SktmController';
+    $name           = 'sktm';
+    $controllers    = (object) [
+        'index'     => $class.'@index',
+        'get'       => $class.'@get',
+        'create'    => $class.'@create',
+        'show'      => $class.'@show',
+        'store'     => $class.'@store',
+        'edit'      => $class.'@edit',
+        'update'    => $class.'@update',
+        'destroy'   => $class.'@destroy',
     ];
 
-    Route::get('/',             $controllers->index)->name('master-sktm.index');
-    Route::get('/create',       $controllers->create)->name('master-sktm.create');
-    Route::get('/{id}',         $controllers->show)->name('master-sktm.show');
-    Route::post('/',            $controllers->store)->name('master-sktm.store');
-    Route::get('/{id}/edit',    $controllers->edit)->name('master-sktm.edit');
-    Route::put('/{id}',         $controllers->update)->name('master-sktm.update');
-    Route::delete('/{id}',      $controllers->destroy)->name('master-sktm.destroy');
-    
+    Route::get('/',             $controllers->index)->name($name.'.index');
+    Route::get('/get',          $controllers->get)->name($name.'.get');
+    Route::get('/create',       $controllers->create)->name($name.'.create');
+    Route::get('/{id}',         $controllers->show)->name($name.'.show');
+    Route::post('/',            $controllers->store)->name($name.'.store');
+    Route::get('/{id}/edit',    $controllers->edit)->name($name.'.edit');
+    Route::put('/{id}',         $controllers->update)->name($name.'.update');
+    Route::delete('/{id}',      $controllers->destroy)->name($name.'.destroy');
 });
